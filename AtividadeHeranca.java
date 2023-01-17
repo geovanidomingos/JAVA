45ee0f1aca0378ebc148b72716356b8580035658
geovanidomingos <geovani.domingos@mercadolivre.com>
2023-01-17 12:30:40 -0300

    ATIVIDADE DE HERANÇA
    ATIVIDADE DE HERANÇA

---
+++ AtividadeHeranca.java
@@ -0,0 +1,10 @@
+package Herança;
+
+public class AtividadeHeranca {
+
+    public static void main(String[] args) {
+        PessoaJuridica pessoaJuridica = new PessoaJuridica("21584595247", "Fernando Dos Santos",28,"Luana Peixote","1235468521396/0");
+
+        pessoaJuridica.Visu();
+    }
+}
